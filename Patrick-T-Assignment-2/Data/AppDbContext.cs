a3696396beeb28b96e39344a3317fca42f116a29
tapa991 <tapa@sheridancollege.ca>
2024-11-06 17:45:13 -0500

    Setup sqlite database

---
+++ Patrick-T-Assignment-2/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patrick_T_Assignment_2.Models.Entities;
+
+namespace Patrick_T_Assignment_2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
